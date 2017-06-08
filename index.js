0e346cc224b6c40d4863e1c7fb95d76c77ff4333
浩添 <haotian.zhao@alibaba-inc.com>
2017-06-08 13:22:45 +0800

    first version

--- index.js
+++ index.js
@@ -15,7 +15,68 @@ var Error     = gutil.PluginError;
  * 如果需要重新生成，可以把原来的属性删除掉
  */ 
 
-function deleteSpm(options) {
+exports.addSpmlog = function(options) {
+    var logkey = options.logkey;
+    var filter = options.filter;
+
+    return through2.obj(function(file, enc, cb) {
+
+        if (!logkey) {
+            return cb(new Error('spmlog', '缺少黄金令箭埋点串字段：logkey'));
+        }
+
+        if (!filter || !util.isArray(filter)) {
+            return cb(new Error('spmlog', '缺少元素过滤规则字段：filter'));
+        }
+
+        var html = file.contents.toString('utf8');
+        var filename = file.relative;
+        var clickStr = 'gostr=/' + logkey + ';locaid=d';
+        var rlogkey = new RegExp('(gostr=\\/([^\\"]+);)', 'g');
+
+        // 如果原来就有logkey并且与当前要设置的不相同，则替换成新的
+        html = html.replace(rlogkey, function (m, $1, $2) {
+            if ($2 && $2 != logkey) {
+                return 'gostr=/' + logkey + ';';
+            } else {
+                return $1;
+            }
+        });
+
+        filter.forEach(function (item) {
+            var pattern = sizzle(item);
+            var keyStr = 'data-spm-click'
+            if (!pattern || !util.isArray(pattern)) {
+                return cb(new Error('spmlog', 'spmlog暂不支持此元素过滤器：' + item));
+            }
+            if(pattern[0].indexOf('mouseenter') >= 0){
+                keyStr = 'data-spm-mouseenter'
+            }
+            var rmatch = new RegExp('(' + pattern[0] + '(?!\\s*' + keyStr + '="[^\\"]+"))', 'g');
+            var rvalue = new RegExp(pattern[1]);
+            
+            html = html.replace(rmatch, function (m, $1, $2) {
+                if (rvalue.test($2)) {
+                    var locaid = sutil.genRandom().substring(0, 8);
+                    return $1 + ' ' + keyStr + '="' + clickStr + locaid + '"';
+                } else {
+                    return $1;
+                }
+            });
+        });
+
+        file.contents = new Buffer(html);
+
+        cb(null, file);
+    });
+}
+
+
+/**
+ * 删掉已有的埋点
+ */ 
+
+exports.deleteSpmlog = function() {
     return through2.obj(function(file, enc, cb) {
 
         var html = file.contents.toString('utf8');
@@ -26,12 +87,8 @@ function deleteSpm(options) {
         // 删掉data-spm-click属性
         html = html.replace(spmlogKey, '');
 
-    
-
         file.contents = new Buffer(html);
 
         cb(null, file);
     });
 }
-
-module.exports = deleteSpm;
